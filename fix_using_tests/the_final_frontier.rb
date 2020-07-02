c2e94e92b2d7c4a202c7475e9f1edf17eee79574
Alex Foreman <foreman.alexander@gmail.com>
2020-07-02 20:24:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -6,10 +6,10 @@ end
 
 
 def state_log(star_date)
+  star_date = generate_star_date 
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
   puts state_log(star_date)
-  star_date = generate_star_date
 end
